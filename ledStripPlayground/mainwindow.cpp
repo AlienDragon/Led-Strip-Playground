363354a361397b9ae127a2e0a4435e5eae3cd31f
AlienDragon <oliver.simon333@gmail.com>
2022-11-03 10:36:57 +0000

    changes

--- ledStripPlayground/mainwindow.cpp
+++ ledStripPlayground/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QtWidgets>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    view = new StripViewer(this);
+    this->layout()->addWidget(view);
 }
 
 MainWindow::~MainWindow()
--- ledStripPlayground/mainwindow.h
+++ ledStripPlayground/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "stripviewer.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    StripViewer *view;
 };
 #endif // MAINWINDOW_H
